5ebfd3afd493202a1bd9f8d4c825079323574437
Federico <federicofigini90@gmail.com>
2021-04-13 12:48:12 +0200

    wp

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,15 +3,29 @@ Vue.config.devtools = true;
 let app = new Vue({
     el: "#root",
     data: {
+        uri: "https://api.themoviedb.org/3",
+        api_key: "c3f5703c26f5de5b9e058f16544de17b",
         films: [],
+        tvSeries: [],
         searchFilm: "",
+
     },
     methods: {
         mariello() {
-            axios.get(`https://api.themoviedb.org/3/search/movie?api_key=c3f5703c26f5de5b9e058f16544de17b&query=${this.searchFilm}`)
+            axios.get(`${this.uri}/search/movie?api_key=${this.api_key}&query=${this.searchFilm}`)
                 .then((response) => {
                     this.films = response.data.results
                 });
+            axios.get(`${this.uri}/search/tv?api_key=${this.api_key}&query=${this.searchFilm}`)
+                .then((response) => {
+                    this.tvSeries = response.data.results
+                });
+        },
+        stars(vote) {
+            let splitVote = vote.vote_average / 2
+            let star = Math.round(splitVote)
+            console.log(Number(star));
+            return Number(star)
         }
     },
     mounted() {},
